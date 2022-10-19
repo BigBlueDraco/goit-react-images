c9e88bb151910b8bc3ca9d594b1f7b78de4462aa
Dafarlog <Dafarlog@gmail.com>
2022-10-19 21:45:46 +0300

    add /

--- src/services/pixabayAPI.js
+++ src/services/pixabayAPI.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export async function getPictures(query = '', page = 1, per_page = 12) {
   const KEY = '29897039-6335e8959bf94ffd3acb5a033';
   const res = await axios.get(
-    `https://pixabay.com/api?q=${query}&per_page=${per_page}&page=${page}`,
+    `https://pixabay.com/api?q=${query}&per_page=${per_page}&page=${page}/`,
     {
       params: {
         key: KEY,
