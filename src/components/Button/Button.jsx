145efdb11e77400a9f66a91092b34a6071bd26ed
Dafarlog <Dafarlog@gmail.com>
2022-10-19 20:03:20 +0300

    ref button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,17 +1,12 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
 
-export class Button extends Component {
-  //   static propTypes = {second: third}
-
-  render() {
-    return (
-      <button className="Button" onClick={e => this.props.func(e)}>
-        Load more
-      </button>
-    );
-  }
-}
+export const Button = func => {
+  return (
+    <button className="Button" onClick={e => this.props.func(e)}>
+      Load more
+    </button>
+  );
+};
 Button.propTypes = {
   func: PropTypes.func,
 };
